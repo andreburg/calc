1d475cac83bc6fc24b2dd6b456f9e50f1f0fdda2
Andre <47895843+andreburg@users.noreply.github.com>
2025-02-02 18:46:39 +0200

    style: Add finishing touches to styling

--- src/application/core/component/header.ts
+++ src/application/core/component/header.ts
@@ -3,6 +3,9 @@ import { createElement } from "../../../utils/virtual-element";
 export function header() {
   return createElement({
     type: "header",
-    props: { children: [createElement({ type: "span", props: { innerText: "calculator", className: "title" } })] },
+    props: {
+      children: [createElement({ type: "span", props: { innerText: "...", className: "title" } })],
+      className: "brutal-container brutal-shadow-container",
+    },
   });
 }
