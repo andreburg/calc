d466d8863ea36d6fa8571a2978893bbc92e0439b
Andre <47895843+andreburg@users.noreply.github.com>
2025-02-02 10:29:14 +0200

    fix: Fix dropdown event bubbling issue
    Fix dropdown event bubbling issue preventing the dropdown recognizing button clicks from children by adding {capture: true} it makes the event listener run the capture phase instead

--- src/application/core/component/dropdown/dropdown.ts
+++ src/application/core/component/dropdown/dropdown.ts
@@ -3,19 +3,23 @@ import { StateObject } from "../../../../utils/state-object";
 import { dropdownTrigger } from "./dropdown-trigger";
 import { dropdownContent } from "./dropdown-content";
 
-export function dropdown({ label, child }: { label: string; child: HTMLElement | string }) {
+export function dropdown({ label, child, className }: { label: string; child: HTMLElement | string; className?: string }) {
   const open = new StateObject(false);
   const element = createElement({
     type: "div",
     props: {
       children: [dropdownTrigger({ label, open }), dropdownContent({ open, child })],
-      className: "dropdown",
+      className: "dropdown " + className,
     },
   });
 
-  window.addEventListener("click", (e) => {
-    if (!element.contains(e.target as Node)) open.value = false;
-  });
+  window.addEventListener(
+    "click",
+    (e) => {
+      if (!element.contains(e.target as Node)) open.value = false;
+    },
+    { capture: true }
+  );
 
   return element;
 }
